5d3964d4bd850a93debcaadb80ab510a4a87a0ee
Greco Giovanni <Giovanni.Greco@nttdata.com>
2017-09-27 12:39:35 +0200

    BottomSheetDialog implementation

--- POC_BottomSheetDialog/Droid/MainActivity.cs
+++ POC_BottomSheetDialog/Droid/MainActivity.cs
@@ -1,27 +1,56 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
+using Android.Support.Design.Widget;
 
 namespace POC_BottomSheetDialog.Droid
 {
     [Activity(Label = "POC_BottomSheetDialog", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        BottomSheetDialog mBottomSheetDialog;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            mBottomSheetDialog = new BottomSheetDialog(this);
+            View sheetView = LayoutInflater.Inflate(Resource.Layout.bottom_sheet_dialog, null);
+            mBottomSheetDialog.SetContentView(sheetView);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            mBottomSheetDialog.FindViewById(Resource.Id.button_close).Click += (sender, ea) => {
+                mBottomSheetDialog.Hide();
+			};
+
+			mBottomSheetDialog.FindViewById(Resource.Id.textView_action1).Click += (sender, ea) => {
+                showToast(((TextView)sender).Text);
+			};
+
+            mBottomSheetDialog.FindViewById(Resource.Id.textView_action2).Click += (sender, ea) => {
+            	showToast(((TextView)sender).Text);
+            };
+
+			mBottomSheetDialog.FindViewById(Resource.Id.textView_action3).Click += (sender, ea) => {
+				showToast(((TextView)sender).Text);
+			};
+
+			mBottomSheetDialog.FindViewById(Resource.Id.textView_action4).Click += (sender, ea) => {
+				showToast(((TextView)sender).Text);
+			};
+
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Text = "Open bottom sheet dialog";
+			button.Click += delegate { mBottomSheetDialog.Show(); };
+			
         }
+
+		void showToast(string text)
+		{
+            Toast.MakeText(this, "Action " + text, ToastLength.Short).Show();
+		}
     }
 }
 
